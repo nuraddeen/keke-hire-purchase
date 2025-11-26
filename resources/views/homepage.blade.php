7101bdf97fc4924ba9b539fdaa0badeef08254d5
nuraddeen <mbellogarindau@gmail.com>
2025-11-26 17:27:58 +0100

    auth scaffolding

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Keke Hire Purchase | Home</title>
+</head>
+<body>
+
+    <h1>Welcome to Keke Hire Purchase</h1>
+    <p>Your trusted partner for all your hire purchase needs.</p>
+    
+</body>
+</html>
